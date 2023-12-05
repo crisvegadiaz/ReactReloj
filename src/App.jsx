99817f9a436c2d9afaa919af9e269efa7ccefb7c
crisvegadiaz <alexander85vega@gmail.com>
2023-12-04 22:03:20 -0300

    Avance de la hora

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [sessionLength, setSessionLength] = useState(25 * 60); 
+  const [breakLength, setBreakLength] = useState(5 * 60); 
+  const [timer, setTimer] = useState(null);
+
+  useEffect(() => {
+    return () => {
+      clearInterval(timer);
+    };
+  }, [timer]);
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  };
+
+  const startTimer = () => {
+    const intervalId = setInterval(() => {
+      setSessionLength((prevSession) => prevSession - 1);
+    }, 1000);
+    setTimer(intervalId);
+  };
+
+  const pauseTimer = () => {
+    clearInterval(timer);
+  };
+
+  const resetTimer = () => {
+    setSessionLength(25 * 60);
+    setBreakLength(5 * 60);
+    clearInterval(timer);
+  };
 
   return (
-    <>
+    <main className="cuerpo">
+      <h1 className="hora">{formatTime(sessionLength)}</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={startTimer}>
+          <i className="bi bi-play-circle-fill"></i>
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={pauseTimer}>
+          <i className="bi bi-pause-circle-fill"></i>
+        </button>
+        <button onClick={resetTimer}>
+          <i className="bi bi-skip-backward-circle-fill"></i>
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </main>
+  );
+};
 
-export default App
+export default App;
